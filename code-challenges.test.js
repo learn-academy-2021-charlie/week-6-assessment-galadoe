cf5df2b51e76bb1aac2fdae15ca9da7ce93e0344
galadoe <kgaladoe@gmail.com>
2021-08-22 20:21:11 -0700

    last challenge question completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,6 +17,12 @@
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their names capitalized.
 
 // a) Create a test with an expect statement using the variable provided.
+describe('sentence', () => {
+    test('should make a sentece about each person with their names', () => {
+        expect(sentence(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthur Dent is a radio employee."])
+    })
+
+})
 
 var people = [
   { name: "ford prefect", occupation: "hitchhiker" },
@@ -29,32 +35,55 @@ var people = [
 
 // b) Create the function that makes the test pass.
 
-
+const sentence = () => {
+  console.log("test");
+}
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe('numbers', () => {
+    test('should only return the remainders of the numbers divided by 3', () => {
+      var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+      // Expected output: [ 2, 0, -1, 0 ]
+      var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+      // Expected output: [ 2, 1, -1 ]
+      expect(numbers(hodgepodge1)).toEqual([2, 0, -1, 0])
+      expect(numbers(hodgepodge2)).toEqual([2, 1, -1])
+    })
+
+})
 
 
 
 // b) Create the function that makes the test pass.
 
-
+const numbers = () => {
+  console.log("number test");
+}
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-var cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-var cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
-
+fdescribe('cubedSum', () => {
+    test('should return of sum of all the numbers cubed', () => {
+      var cubeAndSum1 = [2, 3, 4]
+      // Expected output: 99
+      var cubeAndSum2 = [0, 5, 10]
+      // Expected output: 1125
+      expect(cubedSum(cubeAndSum1)).toEqual(99)
+      expect(cubedSum(cubeAndSum2)).toEqual(1125)
+    })
+})
 
 
 // b) Create the function that makes the test pass.
+
+const cubedSum = (arr) => {
+  let newArr = arr.map(value => {
+    return value ** 3
+  })
+  return newArr.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
